2097ff35e962c4a7fce12b10761d0358f928d8ef
Yves NICOLE <yvesnicole@Mac-mini-de-Yves.local>
2026-06-01 15:38:01 +0200

    fix: regenerate tina types locally for cloud validation

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -801,7 +801,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "https://content.tinajs.io/2.4/content/406dd4fa-a226-4261-a62a-82c3f213d9cc/github/main",
+        url: "http://localhost:4001/graphql",
         queries,
       })
     )
